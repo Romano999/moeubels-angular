c224b095711161956e440f1dc9679681899fef80
Romano999 <71527835+Romano999@users.noreply.github.com>
2022-01-12 15:59:47 +0100

    Search page partially made.

---
+++ src/app/search/products-list/result-product/result-product.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from 'src/app/core/models/product';
+
+@Component({
+  selector: 'app-result-product',
+  templateUrl: './result-product.component.html',
+  styleUrls: ['./result-product.component.scss']
+})
+export class ResultProductComponent implements OnInit {
+  @Input() product!: Product;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/search/search.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchComponent } from './search.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ResultProductComponent } from './products-list/result-product/result-product.component';
+import { SearchPaginatorComponent } from './search-paginator/search-paginator.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+@NgModule({
+  declarations: [
+    SearchComponent,
+    SearchBarComponent,
+    ProductsListComponent,
+    ResultProductComponent,
+    SearchPaginatorComponent
+  ],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatPaginatorModule
+  ],
+  exports: [
+    SearchComponent,
+    SearchBarComponent,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    ProductsListComponent,
+    ResultProductComponent,
+    MatPaginatorModule
+  ]
+})
+export class SearchModule { }
