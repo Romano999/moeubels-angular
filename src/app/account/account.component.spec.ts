f980643a9631049f91bfb01e7f769ec9f31e2288
Romano999 <71527835+Romano999@users.noreply.github.com>
2022-08-12 14:07:45 +0200

    Test fixed.

--- src/app/account/account.component.spec.ts
+++ src/app/account/account.component.spec.ts
@@ -1,25 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, getTestBed, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AccountComponent } from './account.component';
 
 describe('AccountComponent', () => {
-  let component: AccountComponent;
-  let fixture: ComponentFixture<AccountComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AccountComponent ]
-    })
-    .compileComponents();
-  });
+  let component: AccountComponent
+  let injector: TestBed;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AccountComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        AccountComponent
+      ]
+    });
+    injector = getTestBed();
+    component = TestBed.inject(AccountComponent);
+    httpMock = TestBed.inject(HttpTestingController)
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
